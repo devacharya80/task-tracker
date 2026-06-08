8f23b2a237d43085f441f2c9f08994700472ee36
devacharya80 <dhanudeva80@gmail.com>
2026-06-08 20:24:36 +0530

    Schema definition

---
+++ backend/models/task.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "completed","cancel"],
+      default: "pending",
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);

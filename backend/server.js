15c6b9672333a4745ffe6c8856cae71e20c9a6dc
devacharya80 <dhanudeva80@gmail.com>
2026-06-08 21:47:49 +0530

    auth-routes

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ const app = express()
 const cors = require('cors')
 
 const connectToDb = require("./config/db.js")
+const authRouter = require("./routes/authRoutes.js")
 
 app.use(cors());
 app.use(express.json())
+
+app.use("/api/auth",authRouter)
 
 const PORT = process.env.PORT || 5000;
 
